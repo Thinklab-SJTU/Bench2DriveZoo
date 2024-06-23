f8f42ed10241dcba1f60220d6d3faddd7ee28074
Xiaosong Jia <81464843+jiaxiaosong1002@users.noreply.github.com>
2024-06-23 09:57:23 +0800

    Update bevformer_base_b2d.py

--- adzoo/bevformer/configs/bevformer/bevformer_base_b2d.py
+++ adzoo/bevformer/configs/bevformer/bevformer_base_b2d.py
@@ -355,7 +355,7 @@ evaluation = dict(interval=1, pipeline=test_pipeline)
 runner = dict(type='EpochBasedRunner', max_epochs=total_epochs)
 load_from = 'ckpts/r101_dcn_fcos3d_pretrain.pth'
 log_config = dict(
-    interval=1,
+    interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook')
